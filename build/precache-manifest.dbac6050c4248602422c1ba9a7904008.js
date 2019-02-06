a5d903ec2918abad3afaddcf1b806776e61ff383
Kye Berkeland <kyeberkeland@yahoo.com>
2019-02-06 13:47:15 -0600

    Secure the sub

---
+++ build/precache-manifest.dbac6050c4248602422c1ba9a7904008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e08aeef036345fa3b010",
+    "url": "/static/css/main.6ebeadf9.chunk.css"
+  },
+  {
+    "revision": "e08aeef036345fa3b010",
+    "url": "/static/js/main.e08aeef0.chunk.js"
+  },
+  {
+    "revision": "6a24ebe9e34e7a8b8a0c",
+    "url": "/static/js/1.6a24ebe9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  },
+  {
+    "revision": "4b63979710d76c3b643215bb4f4e2bf2",
+    "url": "/index.html"
+  }
+];
