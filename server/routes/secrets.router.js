fa1f1aeef5880f1334e9352a38b484d03fe54aa7
Kye Berkeland <kyeberkeland@yahoo.com>
2019-02-06 13:35:16 -0600

    Complete base

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -4,12 +4,18 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
+    if (req.isAuthenticated()) {
+        let queryText = `SELECT * FROM "secret"
+                         WHERE "secrecy_level" <= $1;`;
+        pool.query(queryText,[req.user.clearance_level])
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT for secrets:', error);
+                res.sendStatus(500);
         });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
