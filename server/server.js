d09a5c73323359444b53949a6a741de08bfd3bd4
Kye Berkeland <kyeberkeland@yahoo.com>
2019-02-07 11:35:56 -0600

    Create server route for returnusers

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const secretsRouter = require('./routes/secrets.router');
+const returnUsersRouter = require('./routes/returnusers.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -26,6 +27,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/secrets', secretsRouter);
+app.use('/api/returnusers', returnUsersRouter);
 
 // Serve static files
 app.use(express.static('build'));
